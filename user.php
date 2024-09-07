5049be4b3222f9bb2aabb26ce1fae81055a51e76
NikaelNP <81631240nikael@gmail.com>
2024-09-07 13:52:07 -0300

    commit final do dia

--- user.php
+++ user.php
@@ -1,15 +1,21 @@
 <?php
 require_once 'database.php';
-
+// arquivo de criaçao do usuario no bd
 class User {
     private $conn;
     private $chave;
 
+    //metodo construtor é chamado automaticamente quando um objeto da classe User é criado
     public function __construct() {
+        //Aqui, um novo objeto da classe Database é criado.
+        // O método connect() da classe Database é chamado automaticamente pelo construtor da classe Database,
+        // estabelecendo uma conexão com o banco de dados. A propriedade conn do objeto Database
+        // (que contém a conexão com o banco de dados) é então atribuída à propriedade $conn da classe User.
         $this->conn = (new Database())->conn;
         $this->chave = getenv('CHAVE_CRIPTOGRAFIA');
     }
 
+    //funçao para registro do usuario encriptografando os dados pessoais como senha e cpf
     public function register($nome, $email, $senha, $account_type, $cpf_cnpf, $endereco) {
         $senhaCriptografada = openssl_encrypt($senha, 'aes-256-cbc', $this->chave, 0, str_repeat('0', 16));
         $cpfCnpjCriptografado = openssl_encrypt($cpf_cnpf, 'aes-256-cbc', $this->chave, 0, str_repeat('0', 16));
@@ -20,6 +26,7 @@ class User {
         return $stmt->execute();
     }
 
+    //funçao para login desincriptografando a senha 
     public function login($email, $senha) {
         $sql = "SELECT senha FROM usuario WHERE email = ?";
         $stmt = $this->conn->prepare($sql);
